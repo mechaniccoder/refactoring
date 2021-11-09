a472e905db4ddf46bdf41d556e64cb11f18a8c99
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-09 23:46:38 +0900

    Replace control flag with break - pipe

--- replace_control_flag_with_break/ex1.js
+++ replace_control_flag_with_break/ex1.js
@@ -1,14 +1,5 @@
 checkForMiscreants(people);
 
 function checkForMiscreants(people) {
-  for (const p of people) {
-    if (p === "joker") {
-      sendAlert();
-      return;
-    }
-    if (p === "saruman") {
-      sendAlert();
-      return;
-    }
-  }
+  if (people.some((p) => ["joker", "saruman"].includes(p))) sendAlert();
 }
