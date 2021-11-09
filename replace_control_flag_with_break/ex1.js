922131b90aa2974a86a5adda674ed7b3533314b2
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-09 23:41:12 +0900

    Replace control flag with break - boilerplate

---
+++ replace_control_flag_with_break/ex1.js
@@ -0,0 +1,13 @@
+let found = false;
+for (const p of people) {
+  if (!found) {
+    if (p === "joker") {
+      sendAlert();
+      found = true;
+    }
+    if (p === "saruman") {
+      sendAlert();
+      found = true;
+    }
+  }
+}
