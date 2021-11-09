512fbc62fe4f89c34c0fe19809334ed1ad2cc388
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-09 23:42:40 +0900

    Replace control flag with break - extract

--- replace_control_flag_with_break/ex1.js
+++ replace_control_flag_with_break/ex1.js
@@ -1,13 +1,17 @@
-let found = false;
-for (const p of people) {
-  if (!found) {
-    if (p === "joker") {
-      sendAlert();
-      found = true;
-    }
-    if (p === "saruman") {
-      sendAlert();
-      found = true;
+checkForMiscreants(people);
+
+function checkForMiscreants(people) {
+  let found = false;
+  for (const p of people) {
+    if (!found) {
+      if (p === "joker") {
+        sendAlert();
+        found = true;
+      }
+      if (p === "saruman") {
+        sendAlert();
+        found = true;
+      }
     }
   }
 }
