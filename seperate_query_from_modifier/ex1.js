98c7f10453bf3324152cef83b22d8fc60fe866ad
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-15 22:54:07 +0900

    Seperate query from modifier - clone

---
+++ seperate_query_from_modifier/ex1.js
@@ -0,0 +1,27 @@
+function alertForMiscreant(people) {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+      return "조커";
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+      return "사루만";
+    }
+  }
+  return "";
+}
+
+function findMiscreant(people) {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+      return "조커";
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+      return "사루만";
+    }
+  }
+  return "";
+}
