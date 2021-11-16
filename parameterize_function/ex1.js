549b625e67e36e93b1c1ab06cbf57c520f9c5bb1
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-16 23:26:42 +0900

    Parameterize_function - replace function

--- parameterize_function/ex1.js
+++ parameterize_function/ex1.js
@@ -1,17 +1,11 @@
 function baseCharge(usage) {
   if (usage < 0) return usd(0);
-  const amount = bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
-  return usd(amount);
-}
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
+  const amount =
+    withinBand(usage, 0, 100) * 0.03 + withinBand(usage, 100, 200) * 0.05 + withinBand(usage, 200, Infinity) * 0.07;
+  return usd(amount);
 }
 
 function withinBand(usage, bottom, top) {
   return usage > bottom ? Math.min(usage, top) - bottom : 0;
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
 }
