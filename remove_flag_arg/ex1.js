40fdb93db5822d520bb6718b9faea37e2b1450fb
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-18 23:30:03 +0900

    Remove flag argument - divide condition

--- remove_flag_arg/ex1.js
+++ remove_flag_arg/ex1.js
@@ -4,16 +4,24 @@ aShipment.deliveryDate = deliveryDate(anOrder, false);
 
 function deliveryDate(anOrder, isRush) {
   if (isRush) {
-    let deliveryTime;
-    if (["ma", "ct"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (["ny", "nh"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    return rushDeliveryDate(anOrder);
   } else {
-    let deliveryTime;
-    if (["ma", "ct"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (["ny", "nh"].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return regularDeliveryDate(anOrder);
   }
+}
+
+function rushDeliveryDate(anOrder) {
+  let deliveryTime;
+  if (["ma", "ct"].includes(anOrder.deliveryState)) deliveryTime = 1;
+  else if (["ny", "nh"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else deliveryTime = 3;
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function regularDeliveryDate(anOrder) {
+  let deliveryTime;
+  if (["ma", "ct"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else if (["ny", "nh"].includes(anOrder.deliveryState)) deliveryTime = 3;
+  else deliveryTime = 4;
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
