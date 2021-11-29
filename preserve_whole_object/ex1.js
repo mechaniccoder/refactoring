30a55296e140c52f82b5b8168511c6678d5254ad
mechaniccoder <yuseunghwan94@gmail.com>
2021-11-29 22:16:08 +0900

    Preserve whole object - boilerplate

---
+++ preserve_whole_object/ex1.js
@@ -0,0 +1,11 @@
+const low = aRoom.daysTempRange.low;
+const high = aRoom.daysTempRange.high;
+if (!aPlan.withinRange(low, high)) {
+  alerts.push("온도가 벗어났습니다.");
+}
+
+class HeatingPlan {
+  withinRange(bottom, top) {
+    return bottom >= this._temperature.low && top <= this._temperature.high;
+  }
+}
