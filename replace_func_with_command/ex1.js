a33529400e9332a1e6e67d4bf373ca788b39fb65
mechaniccoder <yuseunghwan94@gmail.com>
2021-12-07 22:41:48 +0900

    Replace func with command - extract to class

--- replace_func_with_command/ex1.js
+++ replace_func_with_command/ex1.js
@@ -1,19 +1,25 @@
-function score(candidate, mediacalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMediacalRiskFlag = false;
+function score(candidate, medicalExam, scoringGuide) {
+  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-  if (mediacalExam.isSmoker) {
-    healthLevel += 10;
-    highMediacalRiskFlag = true;
+class Scorer {
+  execute(candidate, medicalExam, scoringGuide) {
+    let result = 0;
+    let healthLevel = 0;
+    let highMediacalRiskFlag = false;
+
+    if (medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMediacalRiskFlag = true;
+    }
+
+    let certificationGrade = "regular";
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = "low";
+      healthLevel -= 5;
+    }
+
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = "low";
-    healthLevel -= 5;
-  }
-
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
