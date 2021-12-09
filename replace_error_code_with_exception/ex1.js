8656418f707adb9af0df2ad3617a089c1683212b
mechaniccoder <yuseunghwan94@gmail.com>
2021-12-09 22:35:52 +0900

    Replace error code with exception - custom error class

--- replace_error_code_with_exception/ex1.js
+++ replace_error_code_with_exception/ex1.js
@@ -1,12 +1,12 @@
 function localShippingRules(country) {
   const data = countryData.shippingRules[country];
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw OrderProcessingError(-23);
 }
 
 function calculateShippingCosts(anOrder) {
   const shippingRules = localShippingRules(anOrder.country);
-  if (shippingRules < 0) return shippingRules;
+  if (shippingRules < 0) throw new Error("오류 존재");
 }
 
 // 호출차
@@ -15,6 +15,21 @@ let status;
 try {
   status = calculateShippingCosts(orderData);
 } catch (e) {
-  throw e;
+  if (e instanceof OrderProcessingError) {
+    errorList.push({ order: OrderData, errorCode: e.code });
+  } else {
+    throw e;
+  }
 }
 if (status < 0) errorList.push({ order: orderData, errorCode: status });
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super(`주문 처리 오류 ${errorCode}`);
+    this.code = errorCode;
+  }
+
+  get name() {
+    return OrderProcessingError.name;
+  }
+}
