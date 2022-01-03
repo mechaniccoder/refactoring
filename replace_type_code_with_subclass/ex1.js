b0afff9b4cf0df9b121afd84d09f1f1f1d367cb5
mechaniccoder <yuseunghwan94@gmail.com>
2022-01-03 23:24:22 +0900

    Replace type code with subclass - create other subclasses

--- replace_type_code_with_subclass/ex1.js
+++ replace_type_code_with_subclass/ex1.js
@@ -26,10 +26,26 @@ class Engineer extends Employee {
   }
 }
 
+class Manager extends Employee {
+  get type() {
+    return "manager";
+  }
+}
+
+class SalesPerson extends Employee {
+  get type() {
+    return "salesperson";
+  }
+}
+
 function createEmplyee(name, type) {
   switch (type) {
     case "engineer":
       return new Engineer(name, type);
+    case "manager":
+      return new Manager(name, type);
+    case "salesperson":
+      return new SalesPerson(name, type);
   }
   return new Employee(name, type);
 }
