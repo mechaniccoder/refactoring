58aeca223fd3ebdb3459f282695ba8f893ac2a57
mechaniccoder <yuseunghwan94@gmail.com>
2022-04-08 00:21:18 +0900

    extract function - no local variable

--- 2nd_reading/extract_func.js
+++ 2nd_reading/extract_func.js
@@ -1,21 +1,31 @@
 function printOwing(invoice) {
     let outstanding = 0;
 
-    console.log("**");
-    console.log("**고객채무**");
-    console.log("**");
+    printBanner()
 
     for (const o of invoice.orders) {
         outstanding += o.amount;
     }
 
-    const today = Clock.today;
-    invoice.dueDate = new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate() + 30
-    );
+    recordDueDate(invoice)
+    printDetail(invoice, outstanding)
 
-    console.log(`고객명: ${invoice.customer}`);
+    function printBanner() {
+        console.log("**");
+        console.log("**고객채무**");
+        console.log("**");
+    }
+
+    function printDetail(invoice, outstanding) {
+        console.log(`고객명: ${invoice.customer}`);
+        console.log(`채무액 ${outstanding}`)
+    }
+
+    function recordDueDate(invoice) {
+        const today = Clock.today;
+        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    }
 }
+
 module.exports = {};
