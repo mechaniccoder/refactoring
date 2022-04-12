3706b9dc9c3a3d8ac0cbcd1732d7e9fb29b56c0d
mechaniccoder <yuseunghwan94@gmail.com>
2022-04-12 23:01:48 +0900

    Change function declaration - migration procedure

--- 2nd_reading/change_function_declaration.js
+++ 2nd_reading/change_function_declaration.js
@@ -3,6 +3,14 @@
  * 1. 간단한 절차
  * 2. 마이그레이션 절차
  */
+
+/**
+ * @deprecated change function declaration to circumference
+ */
+function circum(radius) {
+    return circumference(radius)
+}
+
 function circumference(radius) {
     return 2 * Math.PI * radius
 }
