cf97a174c79b292e927fa1b06244737a524161be
mechaniccoder <yuseunghwan94@gmail.com>
2022-08-12 23:51:49 +0900

    Replace super class with delegate

---
+++ 2nd_reading/replace_superclass_with_delegate.js
@@ -0,0 +1,47 @@
+class CatalogItem {
+    constructor(id, title, tags) {
+        this._id = id;
+        this._title = title;
+        this._tags = tags
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    hasTag(arg) {
+        return this._tags.includes(arg)
+    }
+}
+
+class Scroll {
+    constructor(id, title, tags, dateLastCleaned) {
+        this._lastCleaned = dateLastCleaned;
+        this._catalogItem = new CatalogItem(id, title, tags);
+    }
+
+    get id() {
+        return this._catalogItem.id;
+    }
+
+    get title() {
+        return this._catalogItem.title
+    }
+
+    hasTag(aString) {
+        return this._catalogItem.hasTag(aString)
+    }
+
+    needCleaning(targetDate) {
+        const threshold = this.hasTag('revered') ? 700 : 1500;
+        return this.daySinceLastCleaning(targetDate) > threshold;
+    }
+
+    daySinceLastCleaning(targetDate) {
+        return this._lastCleaned.until(targetDate, ChronoUnit.DAYS)
+    }
+}
